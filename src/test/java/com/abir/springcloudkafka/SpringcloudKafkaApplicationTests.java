7a5622e4f7cfd3de4fa5e8112a3958ce59db7f27
abir <benabdellah.abir.2000@gmail.com>
2024-01-23 09:16:35 +0100

    v1

---
+++ src/test/java/com/abir/springcloudkafka/SpringcloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abir.springcloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
